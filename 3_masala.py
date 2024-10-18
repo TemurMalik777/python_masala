74adc422be31871b3b0552319f77d9e06b00a4c2
TemurMalik777 <qushaqovjabor@gmail.com>
2024-10-18 19:41:14 +0500

    sdg

---
+++ 3_masala.py
@@ -0,0 +1,72 @@
+import os 
+
+# s = os.getcwd()
+# print(s)
+
+# n = os.path.basename(s)
+# print(n)
+
+# n = os.path.basename(s+'/file_1.py')
+# print(n)
+
+# s = os.path.dirname(r'D:\Paython_4_darslik')
+# print(s)
+
+# path = os.getcwd
+# s = os.path.dirname(r'')
+
+# s = os.path.isabs(r'C:\Users\User\Python\Data')
+# print(s)
+
+# path = os.getcwd()
+# s = os.path.dirname(path)
+# print(s)
+
+# s = os.path.isabs(r'C:\Users\User\Python\Data')
+# print(s)
+
+# s = os.path.isabs(r'C:Users\User\Python\Data')
+# print(s)
+
+# s = os.path.isfile(r'C:\Users\User\Python\Data')
+# if s:
+#     print("fayil mavjud")
+# else:
+#     print("mavjud emas")
+
+# s = os.path.isfile(r'C:\Users\User\Python\Data')
+# if s:
+#     print("Fayil mavjud")
+# else:
+#     print("mavjud emas")
+
+# path = os.getcwd()
+
+# s = os.path.normcase(path)
+# print(s)
+
+# s = os.path.normcase('C:/Users/User/Python/Data')
+# print(s)
+
+# my_cdw = os.getcwd()
+# file_name = '1_masala.py'
+# pat = my_cdw + '\\' +file_name
+
+# path = os.path.join(my_cdw, file_name)
+# print(path)
+
+# my_cwd = os.getcwd()
+# #new_dir = 'Solutions'
+# path = os.path.join(my_cwd, 'yangi')
+# os.mkdir(path)
+# print(os.listdir())
+
+# s = os.getcwd()
+# print(s)
+# try:
+#     os.chdir(s+r'\1_dars_Python')
+#     s = os.getcwd()
+#     print(s)
+# except:
+#     print("Bunday yo'l mavjud emas")
+
---
+++ 5_masala.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+dest_path = os.getcwd()
+if os.path.exists(dest_path):
+    print(dest_path)
+
+source_path = r'D:\Paython_4_darslik\pythonProject1\8_dars_Python'
+if os.path.exists(source_path):
+    print(source_path)
+
+try:
+    copy_file = shutil.move(source_path, dest_path)
+    print(f"Fayl nusxalandi: {copy_file}")
+except FileNotFoundError as e:
+    print(f"Xatolik: {e}")
+except Exception as e:
+    print(f'Boshqa xatolik: {e}')
